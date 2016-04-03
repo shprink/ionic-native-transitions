4009aa845f37d1e3fac0d36403d75b2b7e5bf0ba
Thomas SALVETAT <thomas.salvetat@outlook.com>
2016-04-03 13:44:14 +0200

    Deletion of the changes about dist files

--- dist/ionic-native-transitions.js
+++ dist/ionic-native-transitions.js
@@ -124,7 +124,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	exports['default'] = function () {
 	    'ngInject';
 	
-	    $get.$inject = ["$log", "$ionicConfig", "$rootScope", "$timeout", "$state", "$location", "$ionicHistory", "$ionicPlatform"];
 	    var enabled = true,
 	        $stateChangeStart = null,
 	        $stateChangeSuccess = null,
@@ -156,6 +155,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        backInOppositeDirection: false // Disable default back transition and uses the opposite transition to go back
 	    };
 	
+	    $get.$inject = ["$log", "$ionicConfig", "$rootScope", "$timeout", "$state", "$location", "$ionicHistory", "$ionicPlatform"];
 	    return {
 	        $get: $get,
 	        enable: enable,
@@ -273,7 +273,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	            unregisterToRouteEvents: unregisterToRouteEvents,
 	            registerToStateChangeStartEvent: registerToStateChangeStartEvent,
 	            unregisterToStateChangeStartEvent: unregisterToStateChangeStartEvent,
-	            disablePendingTransition: disablePendingTransition,
 	            locationUrl: locationUrl,
 	            stateGo: stateGo,
 	            goBack: goBack
@@ -299,10 +298,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	                return;
 	            }
 	            unregisterToStateChangeStartEvent();
-	            var locationPromise = $location.url(url);
+	            $location.url(url);
 	            transition(transitionOptions);
-	
-	            return locationPromise;
 	        }
 	
 	        /**
@@ -333,10 +330,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	                return;
 	            }
 	            unregisterToStateChangeStartEvent();
-	            var statePromise = $state.go(state, stateParams, stateOptions);
+	            $state.go(state, stateParams, stateOptions);
 	            transition(transitionOptions);
-	
-	            return statePromise;
 	        }
 	
 	        /**
@@ -492,21 +487,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	            registerToStateChangeStartEvent();
 	        }
 	
-	        function disablePendingTransition() {
-	            // If native transition support cancelling transition (> 0.6.4), cancel pending transition
-	            if (angular.isFunction(window.plugins.nativepagetransitions.cancelPendingTransition)) {
-	                window.plugins.nativepagetransitions.cancelPendingTransition();
-	                registerToStateChangeStartEvent();
-	            } else {
-	                executePendingTransition();
-	            }
-	        }
-	
 	        function registerToRouteEvents() {
 	            unregisterToRouteEvents();
 	            registerToStateChangeStartEvent();
 	            // $stateChangeSuccess = $rootScope.$on('$stateChangeSuccess', executePendingTransition);
-	            $stateChangeError = $rootScope.$on('$stateChangeError', disablePendingTransition);
+	            $stateChangeError = $rootScope.$on('$stateChangeError', executePendingTransition);
 	            $stateAfterEnter = $rootScope.$on(getDefaultOptions().triggerTransitionEvent, executePendingTransition);
 	        }
 	
@@ -742,15 +727,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	'use strict';
 	
 	Object.defineProperty(exports, '__esModule', {
-	    value: true
+	  value: true
 	});
 	
 	exports['default'] = ["$ionicNativeTransitions", "$ionicPlatform", "$ionicHistory", "$rootScope", function ($ionicNativeTransitions, $ionicPlatform, $ionicHistory, $rootScope) {
-	    'ngInject';
+	  'ngInject';
 	
-	    $ionicPlatform.ready(function () {
-	        $ionicNativeTransitions.init();
-	    });
+	  $ionicPlatform.ready(function () {
+	    $ionicNativeTransitions.init();
+	  });
 	}];
 	
 	;
